df85b186d8ba6e1577f620bcb8c0e957dce37d89
vignes016-prog <vignes016@gmail.com>
2026-07-20 13:15:58 +0530

    Removed duplicate Desktop folder

--- Desktop/hotelproject/HotelManagementSystem/HotelManagementSystem/src/main/java/com/ty/HotelManagementSystem/Repository/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ty.HotelManagementSystem.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ty.HotelManagementSystem.entity.Room;
-
-public interface RoomRepository extends JpaRepository<Room, Integer> {
-
-}
--- Desktop/hotelproject/HotelManagementSystem/HotelManagementSystem/src/main/java/com/ty/HotelManagementSystem/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ty.HotelManagementSystem.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ty.HotelManagementSystem.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
